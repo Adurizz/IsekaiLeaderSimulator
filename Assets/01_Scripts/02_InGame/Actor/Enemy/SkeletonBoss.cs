0fb407d8d1affca579fde60677214faf5084103f
Adurizz <nickhs988@gmail.com>
2025-07-28 05:08:59 +0900

    적 보스 구현

--- Assets/01_Scripts/02_InGame/Actor/Enemy/SkeletonBoss.cs
+++ Assets/01_Scripts/02_InGame/Actor/Enemy/SkeletonBoss.cs
@@ -1,7 +1,65 @@
+using UnityEngine;
+
 public class SkeletonBoss : Enemy
 {
+    [SerializeField] private ParticleSystem attackRangeEffect;
+    [SerializeField] bool isAttacking;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+#endif
+
     protected override void PerformAttack()
     {
         
+    }
+
+    protected override void Move()
+    {
+        if (Target == null || curState != EEnemyState.Chasing || isAttacking)
+        {
+            return;
+        }
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    public void ActivateAreaEffect()
+    {
+        attackRangeEffect.Play();
+        animator.SetTrigger("Attack");
+        /*
+        isAttacking = true;
+        navMeshAgent.isStopped = true;
+        */
+    }
+
+    public void EndAttack()
+    {
+        /*
+        isAttacking = false;
+        navMeshAgent.isStopped = false;
+        */
+    }
+
+    public void MakeAreaAttack()
+    {
+        ApplyAreaDamage(transform.position, attackRange);
+    }
+
+    public void ApplyAreaDamage(Vector3 position, float range, float damageMultiplier = 1f)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, range, heroLayer);
+
+        foreach (Collider col in colliders)
+        {
+            if (col.gameObject.GetComponent<Actor>().IsDead)
+                continue;
+
+            col.gameObject.GetComponent<Actor>().GetDamage(attack * damageMultiplier);
+        }
     }
 }
